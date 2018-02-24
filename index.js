281694cdf2d9edd77e0fd08f2dbdcb0ba2ab2199
irfankhan18 <irfankhan@hotmail.ca>
2018-02-23 22:06:12 -0500

    boiler plate code

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
+// const users =require("./routes/users");
+// const icons =require("./routes/icons");
+
+const app = express();
+
+//const port = process.env.PORT || 8080; //changed for heruku
+const port = 3002;
+
+//allow other domains to connect
+// const corsOptions = {
+//   origin: 'http://localhost:4200',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors());
+
+//body-parser middleware
+app.use(bodyParser.json());
+//app.use(express.static(__dirname + '/dist'));
+
+//Routes 
+// app.use('/users', users);
+// app.use('/icons', icons);
+
+app.listen(port, () => {
+   console.log('server started on port '+port);
+});
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
+//   app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
